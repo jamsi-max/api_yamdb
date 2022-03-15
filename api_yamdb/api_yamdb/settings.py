dabe2ac72926b622f032498de03ca67357bb6686
Maxim Nemkov <dedau@bk.ru>
2022-03-15 13:47:00 +0300

    intit repo v1

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,8 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api.apps.ApiConfig',
-    'users.apps.UsersConfig',
-    'review.apps.ReviewConfig',
+    'reviews.apps.ReviewsConfig',
 ]
 
 MIDDLEWARE = [
