3617295ab696f3a885e39a3f75c04386cedc10c0
Maxim Nemkov <dedau@bk.ru>
2022-03-19 00:59:25 +0300

    добавил эндпоинт /signup если пользователь уже добавлен то происходит отправка кода подтверждения если нет то добавляется пользователь и происходит отправка на почту токена подтверждения

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,12 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+# from rest_framework_simplejwt.views import TokenObtainPairView
 
-
+# from .views import AuthViewSet, TokenViewSet, UserViewSet
+from .views import SignupView
 
 router = DefaultRouter()
-
+router.register('auth/signup', SignupView, basename='signup')
 
 
 urlpatterns = [
     path('v1/', include(router.urls)),
+    # path('v1/auth/signup/', SignupView.as_view()),
 ]
