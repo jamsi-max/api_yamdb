a1e74af655b5ebf24e6af72b034b0739a1c18217
Руслан Шайдуллин <valter.85@mail.ru>
2022-03-19 18:42:00 +0300

    commit в связи с ошибкой pull

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.IntegerField()
+    category = models.ForeignKey(
+        Category, related_name='titles', on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+class GenreTitle(models.Model):
+    title_id = models.ForeignKey(Title, on_delete=models.SET_NULL)
+    genre_id = models.ForeignKey(Genre, on_delete=models.SET_NULL)
